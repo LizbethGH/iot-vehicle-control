316789feba8e4cc0de99b971fc2476a46a1fc38c
LizbethGH <161343865+LizbethGH@users.noreply.github.com>
2025-06-04 10:10:32 -0600

    Update_corregido_1 app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://44.222.248.238:5000/api/devices";
+const API_URL = "http://3.235.95.58:5000/api/devices";
 const API_ESTADO = "http://44.222.248.238:5000/api/estado_grabacion";
 let ipPublica = "";
 let movimientosGrabados = [];
